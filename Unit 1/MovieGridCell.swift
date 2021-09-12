bfe94c4bf9be7cc6123f9cd3ca1bfb099995b078
sangy1 <56744256+sangy1@users.noreply.github.com>
2021-09-12 13:14:18 -0400

    Unit 2 Commit

---
+++ Unit 1/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Unit 1
+//
+//  Created by SangWon Park on 9/12/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
+	
